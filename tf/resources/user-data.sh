1a9de9eacb5da13e6b1aac9de80e2abb477a5f7d
Kirby Simmons <kirbyjsimmons@gmail.com>
2019-06-23 21:19:24 -0500

    fix: script incorrectly installing docker & docker-compose

--- tf/resources/user-data.sh
+++ tf/resources/user-data.sh
@@ -6,9 +6,13 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt-get install -y docker-ce
 sudo systemctl status docker
+sudo docker -v
 
 # Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$$(uname -s)-$$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.0/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+docker-compose -v
+
+sudo apt-get upgrade
